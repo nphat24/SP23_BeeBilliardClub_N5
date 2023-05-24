7403a2bfbe063a781e2bab4be76698655400c8ca
admin <minhnqph25450@fpt.edu.vn>
2023-05-24 21:31:46 +0700

    ui add and update nv

---
+++ app/src/main/java/mob104/fpoly/myapplication/activities/HomeManagerActivity.java
@@ -0,0 +1,16 @@
+package mob104.fpoly.myapplication.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import mob104.fpoly.myapplication.R;
+
+public class HomeManagerActitity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home_manager_actitity);
+    }
+}
