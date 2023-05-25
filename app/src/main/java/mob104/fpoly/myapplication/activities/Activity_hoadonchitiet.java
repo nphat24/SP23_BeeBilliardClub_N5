2c7b19731b097dcf7abbf365b49bdfd46e3b27d2
bachtoan <toanbui765@gmail.com>
2023-05-25 13:36:32 +0700

    Sửa manifest cho 2 màn hình vừa push

--- app/src/main/java/mob104/fpoly/myapplication/activities/Activity_hoadonchitiet.java
+++ app/src/main/java/mob104/fpoly/myapplication/activities/Activity_hoadonchitiet.java
@@ -7,11 +7,11 @@ import android.os.Bundle;
 import mob104.fpoly.myapplication.R;
 
 
-public class Activity_quanlyhoadon extends AppCompatActivity {
+public class Activity_hoadonchitiet extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_quanlyhoadon);
+        setContentView(R.layout.activity_hoadonchitiet);
     }
 }
