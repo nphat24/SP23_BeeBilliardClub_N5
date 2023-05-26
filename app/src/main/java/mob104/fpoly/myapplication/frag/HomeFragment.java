07b306dfcc850f75002f433a315c60dc9a147858
bachtoan <toanbui765@gmail.com>
2023-05-26 23:37:20 +0700

    tạo layout cho màn hình trang chủ, giao diện trang chủ và item bàn chơi

--- app/src/main/java/mob104/fpoly/myapplication/frag/HomeFragment.java
+++ app/src/main/java/mob104/fpoly/myapplication/frag/HomeFragment.java
@@ -8,8 +8,13 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager2.widget.ViewPager2;
+
+import com.google.android.material.tabs.TabLayout;
+import com.google.android.material.tabs.TabLayoutMediator;
 
 import mob104.fpoly.myapplication.R;
+import mob104.fpoly.myapplication.adapter.HomeAdapter;
 
 public class HomeFragment extends Fragment {
 
@@ -18,6 +23,26 @@ public class HomeFragment extends Fragment {
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_home,container,false);
         // chỗ này của Lão Tôn
+        TabLayout tabLayout = view.findViewById(R.id.tabLayout);
+        ViewPager2 viewPager2 = view.findViewById(R.id.viewPager2);
+
+        HomeAdapter adapter = new HomeAdapter(getActivity());
+        viewPager2.setAdapter(adapter);
+
+        new TabLayoutMediator(tabLayout, viewPager2, new TabLayoutMediator.TabConfigurationStrategy() {
+            @Override
+            public void onConfigureTab(@NonNull TabLayout.Tab tab, int position) {
+               if(position == 0){
+                   tab.setText("Tất cả");
+               }
+               if(position == 1){
+                   tab.setText("Sử dụng");
+               }
+               if(position == 2){
+                   tab.setText("Còn trống");
+               }
+            }
+        }).attach();
         return view;
     }
 }
