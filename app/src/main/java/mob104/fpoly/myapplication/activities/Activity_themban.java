2666f69f1f8bf4cd4302c79b1663024c592cab83
bachtoan <toanbui765@gmail.com>
2023-06-17 04:16:26 +0700

    Sử logic thêm bàn, tránh thêm bàn không có số

--- app/src/main/java/mob104/fpoly/myapplication/activities/Activity_themban.java
+++ app/src/main/java/mob104/fpoly/myapplication/activities/Activity_themban.java
@@ -47,54 +47,59 @@ public class Activity_themban extends AppCompatActivity {
             }
         });
 
+        btn_huy.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+
         btn_them.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                DatabaseReference ref = database.getReference();
+                if(edt_soban.getText().length() < 1){
+                    Toast.makeText(Activity_themban.this, "Tên không được để trống", Toast.LENGTH_SHORT).show();
+                }else{
+                    DatabaseReference ref = database.getReference();
 
-                List<BanchoiModel> banchoiModels = new ArrayList<>();
+                    List<BanchoiModel> banchoiModels = new ArrayList<>();
 
-                ref.child("Table").addValueEventListener(new ValueEventListener() {
+                    ref.child("Table").addValueEventListener(new ValueEventListener() {
 
-                    @Override
-                    public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
-                        boolean checkban = false;
-                        String strban = "Bàn " + edt_soban.getText();
-                        Log.d("bbb", "strban " + strban);
-                        for(DataSnapshot contentDataSnapshot: dataSnapshot.getChildren()){
-                            BanchoiModel banchoiModel = contentDataSnapshot.getValue(BanchoiModel.class);
-                            Log.d("bbb", "banchoiModel" +banchoiModel.getName());
-                            if (banchoiModel.getName().equals(strban)) {
-                                checkban = true;
-                                Toast.makeText(Activity_themban.this, "Tên đã tồn tại", Toast.LENGTH_SHORT).show();
-                                break; // Dừng vòng lặp khi tìm thấy tên đã tồn tại
+                        @Override
+                        public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                            boolean checkban = false;
+                            String strban = "Bàn " + edt_soban.getText();
+                            Log.d("bbb", "strban " + strban);
+                            for(DataSnapshot contentDataSnapshot: dataSnapshot.getChildren()){
+                                BanchoiModel banchoiModel = contentDataSnapshot.getValue(BanchoiModel.class);
+                                Log.d("bbb", "banchoiModel" +banchoiModel.getName());
+                                if (banchoiModel.getName().equals(strban)) {
+                                    checkban = true;
+                                    Toast.makeText(Activity_themban.this, "Tên đã tồn tại", Toast.LENGTH_SHORT).show();
+                                    break; // Dừng vòng lặp khi tìm thấy tên đã tồn tại
+                                }
+                            }
+                            if (!checkban) {
+                                Toast.makeText(Activity_themban.this, "Thêm bàn thành công", Toast.LENGTH_SHORT).show();
+                                DatabaseReference ref = database.getReference("Table");
+                                if (isNumeric(edt_soban.getText()+"") && isNumeric(edt_gia.getText()+"")){
+                                    ref.push().setValue(new BanchoiModel("Bàn " + edt_soban.getText(),"","", edt_gia.getText()+""));
+                                }else{
+                                    Toast.makeText(Activity_themban.this, "Số bàn và giá tiền phải là số", Toast.LENGTH_SHORT).show();
+                                }
                             }
                         }
-                        if (!checkban) {
-                            Toast.makeText(Activity_themban.this, "Thêm bàn thành công", Toast.LENGTH_SHORT).show();
-                            DatabaseReference ref = database.getReference("Table");
-                            if (isNumeric(edt_soban.getText()+"") && isNumeric(edt_gia.getText()+"")){
-                                ref.push().setValue(new BanchoiModel("Bàn " + edt_soban.getText(),"","", edt_gia.getText()+""));
-                            }else{
-                                Toast.makeText(Activity_themban.this, "Số bàn và giá tiền phải là số", Toast.LENGTH_SHORT).show();
-                            }
+
+
+                        @Override
+                        public void onCancelled(@NonNull DatabaseError databaseError) {
+
                         }
-                    }
-
-
-                    @Override
-                    public void onCancelled(@NonNull DatabaseError databaseError) {
-
-                    }
-                });
-
-
-
-
-
-
-
-
+                    });
+                    finish();
+                }
             }
         });
 
